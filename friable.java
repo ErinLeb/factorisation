20672656944478078e657506c30fc909c216715c
Benjamin Decker <benjamin.t.decker@gmail.com>
2024-03-25 10:21:21 +0100

    Crible quadratique

---
+++ friable.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class friable{
+	
+	public static ArrayList<Integer> cribleQuadratique(int borne, int b){
+		if(borne <= 1 || b <= 1){
+			System.out.println("Veuillez entrer une borne et un entier B supérieurs à 1.");
+			return null;
+		}
+		ArrayList<Integer> bfriables = new ArrayList<>();
+		int[] entiers = new int[borne];
+		for(int i = 0; i < borne; i++){
+			entiers[i] = i+1;
+		}
+		for(int i = 2; i <= b; i++){
+			for(int j = i - 1; j < entiers.length; j+=i){
+				while(entiers[j] % i == 0){
+					entiers[j] /= i;
+				}
+			}
+		}
+		for(int i = 0; i < entiers.length; i++){
+			if(entiers[i] == 1){
+				bfriables.add(i + 1);
+			}
+		}
+		return bfriables;
+	}
+
+	public static void main(String[] args){
+		if(args.length != 2){
+			System.out.println("Le programme attend en premier une borne jusqu'à laquelle tester la B-friablilité des entiers et en second un entier B.");
+			return;
+		}
+		int borne = Integer.valueOf(args[0]);
+		int b = Integer.valueOf(args[1]);
+		ArrayList<Integer> bfriables = cribleQuadratique(borne, b);
+		for(int i = 0; i < bfriables.size(); i++){
+			System.out.println(bfriables.get(i) + " est " + b + " friable");
+		}
+	}
+}
